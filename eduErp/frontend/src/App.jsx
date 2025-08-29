b7827ae5f632fdc4d51676efefd45d68d7e6d5ed
DishanGayantha <162087563+DishanGayantha@users.noreply.github.com>
2025-08-29 13:04:06 +0530

    frontend folder structure, react router

--- eduErp/frontend/src/App.jsx
+++ eduErp/frontend/src/App.jsx
@@ -1,15 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <p class="font-bold underline">Hello</p>
-    </>
+    <Routes>
+      <Route path='/' element={"#"} />
+    </Routes>
   )
 }
 
