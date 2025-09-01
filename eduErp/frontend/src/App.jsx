d9784b6efaaef838b447c728dd1198bf3ce41070
Chanuka Navod <chanukanavod17177@gmail.com>
2025-09-01 17:27:48 +0530

    V 1.1.3

--- eduErp/frontend/src/App.jsx
+++ eduErp/frontend/src/App.jsx
@@ -4,14 +4,16 @@ import AddStaff from './pages/AddStaff'
 import DisplayStaff from './pages/DisplayStaff'
 import UpdateStaff from './pages/UpdateStaff'
 import LeaveForm from './pages/LeaveForm'
+import LeaveList from './pages/LeaveList'
 
 const App = () => {
   return (
     <Routes>
       <Route path='/' element={<DisplayStaff />} />
-      <Route path='/add' element={<AddStaff />} />
-      <Route path='/update-staff/:id' element={<UpdateStaff />} />
-      <Route path='/leave-form' element={<LeaveForm />} />
+      <Route path='/staff/add' element={<AddStaff />} />
+      <Route path='/staff/update/:id' element={<UpdateStaff />} />
+      <Route path='/leaves' element={<LeaveList />} />
+      <Route path='/leaves/apply' element={<LeaveForm />} />
     </Routes>
   )
 }
